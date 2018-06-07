b5d98ee435faa16e7851da5966148f61a4a8c177
chadcollins <chadrcollins91@gmail.com>
2018-06-06 21:06:54 -0400

    Removed unneeded function for built in one

--- common/src/main/kotlin/kql/utils/helpers.kt
+++ common/src/main/kotlin/kql/utils/helpers.kt
@@ -6,14 +6,8 @@ import kotlin.reflect.KClass
 fun <T : Any> KClass<T>.zeroParamConstructors() =
         this.constructors.filter { it.parameters.isEmpty() || it.parameters.all { it.isOptional } }
 
-fun <T : Any> KClass<T>.firstZeroParamConstructor() = try {
-    this.zeroParamConstructors().first()
-} catch (exception: NoSuchElementException) {
-    null
-}
-
 fun <T : Any> KClass<T>.stubInstanceAction(action: (it: T) -> Unit) {
-    val stubConstructor = this.firstZeroParamConstructor() ?: throw NoStubConstructorException()
+    val stubConstructor = this.zeroParamConstructors().firstOrNull() ?: throw NoStubConstructorException()
     action(stubConstructor.callBy(HashMap()))
 }
 
